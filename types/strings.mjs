e9fed28f3615920c28c9fdb4c62aabe447bcc411
camwhite <codeandcam@gmail.com>
2018-07-02 19:30:17 -0600

    :heavy_plus_sign: string method examples

--- types/strings.mjs
+++ types/strings.mjs
@@ -10,18 +10,20 @@ import logger from '../util'
 export default (async () => {
 
   // Our first string is the focus of this file
-
   const str = 'this is example string 1' // our first string
   logger(str, 'green') // prints contents of str
 
   // All strings have properties available like length
-
-  const { length } = str // the strings length
+  const { length } = str // the string's length cast into a variable
   logger(length, 'red') // prints 24
 
-  // Don't worry about this too much yet it's a bonus 
-  // but if you can take note do it!
+  // Some of these properties are functions called methods
+  const [ firstChar ] = str.split('') // extract first character
+  const repeated = firstChar.repeat(length) // repeat the first character
+  logger(repeated, 'blue') // logs 24 t's
 
+  // Don't worry about this too much yet it's a bonus 
+  // but take note of it!
   const { promises: { readFile }} = fs // nested destructuring syntax
   const file = await readFile('index.mjs') // await the readFile promise
   logger(file.toString(), 'yellow') // prints file contents
