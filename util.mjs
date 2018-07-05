bd222d5d5dd63c7ee4477da993dddb24ed54042a
camwhite <codeandcam@gmail.com>
2018-07-05 14:13:47 -0600

    :nut_and_bolt: use inspect for logger

--- util.mjs
+++ util.mjs
@@ -1,9 +1,12 @@
 import chalk from 'chalk'
+import util from 'util'
 
 export const logger = (log, color=null) => {
   if (color) {
     console.log(chalk[color](log + '\n\n'))
   } else {
-    console.log(log + '\n\n')
+    const { inspect } = util
+    const inspection = inspect(log)
+    console.log(`${inspection}\n\n`)
   }
 }
